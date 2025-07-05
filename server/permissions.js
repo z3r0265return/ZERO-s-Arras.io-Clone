a65bcef3b1644dcfe41bf08e4c0bd0bd7af1d4fd
z3r0265return <l1081684@outlook.com>
2025-07-05 19:37:59 -0400

    Create permissions.js

---
+++ server/permissions.js
@@ -0,0 +1,39 @@
+module.exports = [ // note to me my key is Z3r0265ssecurekey, because im gonna forget it tommorrow
+  {
+    key: process.env.TOKEN_1,
+    discordID: "0",
+    nameColor: "#ffffff",
+    class: "developer",
+    infiniteLevelUp: true,
+    name: "unnamed#0000",
+    note: "note here",
+  },
+  {
+    key: process.env.TOKEN_2,
+    discordID: "0",
+    nameColor: "#ffffff",
+    class: "developer",
+    infiniteLevelUp: true,
+    name: "unnamed#0000",
+    note: "note here",
+  },
+  {
+    key: process.env.TOKEN_3,
+    discordID: "0",
+    nameColor: "#ffffff",
+    class: "developer",
+    infiniteLevelUp: true,
+    name: "unnamed#0000",
+    note: "note here",
+  },
+  {
+    key: process.env.TOKEN_4,   // add your secret owner token here in .env file
+    discordID: "0",
+    nameColor: "#0000ff",           // owner red flex color
+    class: "owner",
+    infiniteLevelUp: true,
+    name: "Z3r0265",
+    note: "owner access granted",
+    administrator: true,
+  }
+]
